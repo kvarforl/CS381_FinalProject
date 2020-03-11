3b876e114abb4174eff62a59a18caad05aba0fba
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-03-10 17:30:45 -0700

    worked on example?

--- Plates.hs
+++ Plates.hs
@@ -46,49 +46,49 @@ data Value
 	| BV Bool
 	| SV String
 	| Error
-
+    deriving(Eq, Show)
 type Domain = Stack -> Value
   
 
-cmd :: Cmd -> Domain
-cmd Add             (x:y:s) = case (x, y) of
-                                (N i, N j) -> Just (N (i + j) : s)
-                                (S i, S j) -> Just (S (i++j) : s)
-                                (_, _)     -> Nothing  
-cmd Sub             (x:y:s) = case (x,y) of
-                                (N i, N j) -> Just (N (i - j) : s)
-                                (_, _)     -> Nothing  
-cmd Mul             (x:y:s) = case (x,y) of
-                                (N i, N j) -> Just (N (i * j) : s)
-                                (_, _)     -> Nothing
-cmd Greater         (x:y:s) = case (x,y) of
-                                (N i, N j) -> if i > j then Just (B (True) : s) else Just (B (False): s)
-                                (_, _)     -> Nothing
-cmd Equ             (x:y:s) = case (x,y) of
-                                (N i, N j) -> if i == j then Just (B (True) : s) else Just (B (False): s)
-                                (S i, S j) -> if i == j then Just (B (True) : s) else Just (B (False): s)
-                                (_, _)     -> Nothing
-cmd (PushN   n)       stack   = Just(N n: stack)
-cmd (PushB   b)       stack   = Just(B b: stack)
-cmd (PushS   str)     stack   = Just(S str: stack)
-cmd (PushF   prog)    stack   = Just(F prog: stack)
-cmd (Pop)             (x:s)   = Just(s)
---cmd (Loop    p)       (x:s)   = case x of 
---                                    (N int) -> if (int > 0) then for p int s else Just s
-                                    --_ -> Nothing
-cmd (IfElse  pt pf)   (x:s)  = case x of
-                                (B True) ->  prog pt s
-                                (B False)->  prog pf s
-                                _         -> Nothing
-cmd (Call)          (p:s) = case p of
-                                (F cs) -> prog cs s
-                                _ -> Nothing
-cmd (Offset i)        stack  =  offsets i stack []
-cmd (Swap)            (x:y:stack) = Just(y:x:stack)
+-- cmd :: Cmd -> Stack -> sMaybe Stack
+-- cmd Add             (x:y:s) = case (x, y) of
+--                                 (N i, N j) -> Just (N (i + j) : s)
+--                                 (S i, S j) -> Just (S (i++j) : s)
+--                                 (_, _)     -> Nothing  
+-- cmd Sub             (x:y:s) = case (x,y) of
+--                                 (N i, N j) -> Just (N (i - j) : s)
+--                                 (_, _)     -> Nothing  
+-- cmd Mul             (x:y:s) = case (x,y) of
+--                                 (N i, N j) -> Just (N (i * j) : s)
+--                                 (_, _)     -> Nothing
+-- cmd Greater         (x:y:s) = case (x,y) of
+--                                 (N i, N j) -> if i > j then Just (B (True) : s) else Just (B (False): s)
+--                                 (_, _)     -> Nothing
+-- cmd Equ             (x:y:s) = case (x,y) of
+--                                 (N i, N j) -> if i == j then Just (B (True) : s) else Just (B (False): s)
+--                                 (S i, S j) -> if i == j then Just (B (True) : s) else Just (B (False): s)
+--                                 (_, _)     -> Nothing
+-- cmd (PushN   n)       stack   = Just(N n: stack)
+-- cmd (PushB   b)       stack   = Just(B b: stack)
+-- cmd (PushS   str)     stack   = Just(S str: stack)
+-- cmd (PushF   prog)    stack   = Just(F prog: stack)
+-- cmd (Pop)             (x:s)   = Just(s)
+-- --cmd (Loop    p)       (x:s)   = case x of 
+-- --                                    (N int) -> if (int > 0) then for p int s else Just s
+--                                     --_ -> Nothing
+-- cmd (IfElse  pt pf)   (x:s)  = case x of
+--                                 (B True) ->  prog pt s
+--                                 (B False)->  prog pf s
+--                                 _         -> Nothing
+-- cmd (Call)          (p:s) = case p of
+--                                 (F cs) -> prog cs s
+--                                 _ -> Nothing
+-- cmd (Offset i)        stack  =  offsets i stack []
+-- cmd (Swap)            (x:y:stack) = Just(y:x:stack)
 
-offsets :: Int->Stack->Stack-> Maybe Stack
-offsets 0 (x:stack1) stack2 = Just(x:stack2++x:stack1)
-offsets n (x:stack1) stack2 = offsets (n-1) (stack1) (stack2++[x])
+-- offsets :: Int->Stack->Stack-> Maybe Stack
+-- offsets 0 (x:stack1) stack2 = Just(x:stack2++x:stack1)
+-- offsets n (x:stack1) stack2 = offsets (n-1) (stack1) (stack2++[x])
 
  
 
@@ -115,44 +115,62 @@ typeHandle (x:prog) stackType = case typeOf x stackType of
 					stackType' -> typeHandle prog (stackType')
 
 typeOf :: Cmd -> StackType -> StackType
-typeOf Add          (x:y:s) = case (x, y) of
-                                (TInt, TInt) -> (TInt:s)
-                                (TString, TString) -> (TString:s)
-                                (_, _)     -> (TError:s)  
-typeOf Sub          (x:y:s) = case (x,y) of
-                                (TInt, TInt) -> (TInt:s)
-                                (_, _)     -> (TError:s) 
-typeOf Mul          (x:y:s) = case (x,y) of
-                                (TInt, TInt) -> (TInt:s)
-                                (_, _)     -> (TError:s)
-typeOf Greater      (x:y:s) = case (x,y) of
-                                (TInt, TInt) -> (TBool:s)
-                                (_, _)     -> (TError:s)
-typeOf Equ          (x:y:s) = case (x,y) of
-                                (TInt, TInt) -> (TBool:s)
-                                (TString, TString) -> (TBool:s)
-                                (_, _)     -> (TError:s)
+typeOf Add          stack =  case stack of
+                                    (x:y:s) -> case (x, y) of
+                                                (TInt, TInt) -> (TInt:s)
+                                                (TString, TString) -> (TString:s)
+                                                (_, _)     -> (TError:s)
+                                    _       -> (TError:stack)
+                                  
+typeOf Sub          stack =  case stack of
+                                    (x:y:s) -> case (x, y) of
+                                                (TInt, TInt) -> (TInt:s)
+                                                (_, _)     -> (TError:s)
+                                    _       -> (TError:stack)
+                                   
+typeOf Mul          stack =  case stack of
+                                    (x:y:s) -> case (x, y) of
+                                                (TInt, TInt) -> (TInt:s)
+                                                (_, _)     -> (TError:s)
+                                    _       -> (TError:stack)
+                                  
+typeOf Greater      stack =  case stack of
+                                    (x:y:s) -> case (x, y) of
+                                                (TInt, TInt) -> (TBool:s)
+                                                (_, _)     -> (TError:s)
+                                    _       -> (TError:stack)
+                                  
+typeOf Equ          stack =  case stack of
+                                    (x:y:s) -> case (x, y) of
+                                                (TInt, TInt) -> (TBool:s)
+                                                (TString, TString) -> (TBool:s)
+                                                (_, _)     -> (TError:s)
+                                    _       -> (TError:stack)
+                                  
 typeOf (PushN   n)    stack   = (TInt:stack)
 typeOf (PushB   b)       stack   = (TBool:stack)
 typeOf (PushS   str)     stack   = (TString:stack)
-typeOf (PushF   prog)    stack   = case typeHandle prog [] of
+typeOf (PushF   prog)    stack   = case typeHandle prog stack of
 					Nothing -> (TError:stack)
 					Just progStackType -> ((TFunc : progStackType) ++ stack)
-typeOf (Pop)             (x:s)   = s
+typeOf (Pop)             stack   = case stack of
+                                        (x:s) -> s
+                                        (x) -> [] 
+                                        _ -> (TError:stack)
 --cmd (Loop    p)       (x:s)   = case x of 
 --                                    (N int) -> if (int > 0) then for p int s else Just s
 --                                    _ -> Nothing
 
 typeOf (IfElse  pt pf)   (x:s)  = case x of
                                 (TBool) ->  case typeHandle pt [] of
-						Nothing -> (TError:s)
-						Just ptStackType -> case typeHandle pf [] of
-								    	Nothing -> (TError:s)
-									Just pfStackType -> if (ptStackType == pfStackType) then (ptStackType ++ s) else (TError:s)
+                                                Nothing -> (TError:s)
+                                                Just ptStackType -> case typeHandle pf [] of
+                                                                        Nothing -> (TError:s)
+                                                                        Just pfStackType -> if (ptStackType == pfStackType) then (ptStackType ++ s) else (TError:s)
                                 _-> (TError:s)
 typeOf (Call)          (p:s) = case p of
-				TFunc -> s
-				_ -> (TError:s)
+                                    TFunc -> s
+                                    _ -> (TError:s)
 typeOf (Offset i)       stack  =  ((offsetsType i stack []):stack)
 typeOf (Swap)           (x:y:stack) = (y:x:stack)
 
@@ -167,9 +185,12 @@ cmd' :: Cmd -> Stack -> Stack
 cmd' Add             (x:y:s) = case (x, y) of
                                 (N i, N j) -> (N (i + j) : s)
                                 (S i, S j) -> (S (i++j) : s) 
-cmd' Sub             (x:y:s) = (N (x - y) : s) 
-cmd' Mul             (x:y:s) = (N (x * y) : s)
-cmd' Greater         (x:y:s) = if x > y then (B (True) : s) else (B (False): s)
+cmd' Sub             (x:y:s) = case (x, y) of
+                                (N i, N j) -> (N (i - j) : s)
+cmd' Mul             (x:y:s) = case (x, y) of
+                                (N i, N j) -> (N (i * j) : s)
+cmd' Greater         (x:y:s) = case (x, y) of
+                                (N i, N j) -> if i > j then (B (True) : s) else (B (False): s)
 cmd' Equ             (x:y:s) = case (x,y) of
                                 (N i, N j) -> if i == j then (B (True) : s) else (B (False): s)
                                 (S i, S j) -> if i == j then (B (True) : s) else (B (False): s)
@@ -182,10 +203,10 @@ cmd' (Pop)             (x:s)   = (s)
 --                                    (N int) -> if (int > 0) then for p int s else Just s
                                     --_ -> Nothing
 cmd' (IfElse  pt pf)   (x:s)  = case x of
-                                (B True) ->  prog pt s
-                                (B False)->  prog pf s
+                                (B True) ->  progStack pt s
+                                (B False)->  progStack pf s
 cmd' (Call)          (p:s) = case p of
-                                (F cs) -> prog cs s
+                                (F cs) -> progStack cs s
 cmd' (Offset i)        stack  =  offsets' i stack []
 cmd' (Swap)            (x:y:stack) = (y:x:stack)
 
@@ -206,27 +227,34 @@ prog []         (x:s) = case x of
 				S s -> SV s			
 prog (x:p)      stack = prog p (cmd' x stack)
 
+progStack :: Prog -> Stack -> Stack
+progStack []         stack = stack		
+progStack (x:p)      stack = progStack p (cmd' x stack)
+
 
 -- starts a program with an empty stack and statically type checks it
 exec :: Prog -> Value
 exec p = case eval p of 
 		Nothing -> Error
-		(TFunc:s) -> Error
+		Just (TFunc:s) -> Error
 		_ 	-> prog p []
                   
-goodEx :: Int -> Prog
-goodEx x = [PushN 6, PushN x, Equ, IfElse [PushS "YouWin"] [PushS "YouLose"]]
+goodEx1 :: Int -> Prog
+goodEx1 x = [PushN 6, PushN x, Equ, IfElse [PushS "YouWin"] [PushS "YouLose"]]
+
+goodEx2 :: Int -> Prog
+goodEx2 x = [PushF [Offset 0, PushN 0, Equ, IfElse [Pop, Pop, PushN 1] [Offset 1, PushN 1, Offset 2, Sub, Offset 1, Call, Mul]], PushN x, Offset 1, Call]
 
 testEx :: Prog 
-testEx = [PushB True, IfElse [PushN 6] [PushN 7]]
+testEx = [PushN 3, PushF [Offset 0, PushN 1, Equ], PushN 1, Offset 1, Call]
 
 --type error
 --badEx1 :: Prog
---badEx1 = [PushB False, Loop [PushS "this tries to use a boolean as a counter"]]
+badEx1 x = [PushN 6, PushN x, Equ, IfElse [PushS "YouWin", PushN 1] [PushN 0, PushS "YouLose"]]
 
 --stack underflow error
 badEx2 :: Prog
-badEx2 = [PushN 5, PushN 6, Add, Add]
+badEx2 = [PushN 1, PushN 2, Add, Add]
 
 --Syntactic Sugar
 --true :: Prog
